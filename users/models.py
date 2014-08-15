12b8e074ac4573b22bd9456f92aa22d45ef47c9c
aysekucuk <ayse.kucukk@gmail.com>
2014-08-15 17:20:49 +0300

    account model change

--- users/models.py
+++ users/models.py
@@ -3,10 +3,20 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
 
-class MyProfile(UserenaBaseProfile):
+class Accounts(UserenaBaseProfile):
     user = models.OneToOneField(User,
                                 unique=True,
                                 verbose_name=_('user'),
                                 related_name='my_profile')
-    favourite_snack = models.CharField(_('favourite snack'),
-                                       max_length=5)
+    name = models.CharField(max_length = 200, verbose_name = ('name'))
+    surname = models.CharField(max_length = 200 , verbose_name = ('surname'))
+    email = models.EmailField(max_length = 500, verbose_name = ('email'))
+    join_date = models.DateTimeField(auto_now_add = True, blank = True, null = True, verbose_name = ('join date'))
+    slug = models.SlugField(max_length = 250, blank = True, null = True)
+
+    def __init__(self):
+    	return self.name
+
+	def save(self):
+		self.slug = slugify(self.user.username.upper())
+		super(Accounts, self).save()
