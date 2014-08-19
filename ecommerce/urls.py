dfa72b4a35035dcb50f8f821a0a4f3c26f1c8864
aysekucuk <ayse.kucukk@gmail.com>
2014-08-19 17:38:41 +0300

    login page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'ecommerce.views.home', name='home'),
+    url(r'^login$', 'ecommerce.views.login', name='login'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 def home(request):
 	return render(request,"index.html",{})
+
+def login(request):
+	return render(request,"login.html",{})
